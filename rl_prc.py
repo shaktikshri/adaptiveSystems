db1dd8811e5645d542521a7abb13de96eea141ad
Shakti Kumar <shakti.shrivastava13@gmail.com>
2019-10-18 21:16:19 -0400

    adding in the initial distribution

--- rl_prc.py
+++ rl_prc.py
@@ -5,9 +5,16 @@ T = np.array([
     [0.5, 0.5]
 ])
 
+# declaring initial distribution
+v = np.array(
+    [1, 0]
+)
+
 for i in [1,2,3,4,5]:
     T_i = np.linalg.matrix_power(T, i)
-    print('Transition Prob after time t=',i,'\n',T_i)
+    print('Transition Prob after time k=',i,'\n',T_i)
+    vti = np.dot(v, T_i)
+    print('Prob of being in a specific state after',i,'iterations = \n', vti)
 # the transition probability converges to array([
 #           [0.83333333, 0.16666667],
 #           [0.83333333, 0.16666667]
@@ -16,3 +23,5 @@ for i in [1,2,3,4,5]:
 # system will remain in s0 with a prob of 83.3% and move to s1 16.7% of the time
 # Upon moving to s1, it will stay in s1 16.7% of the time, and move to s0
 # with 83.3% probability
+# this is ind. of the initial state as we can see with the initial state vector.
+# thus this Markov process has converged.
