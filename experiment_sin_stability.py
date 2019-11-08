25e663bbe4fb1e6e6db0f68f286ee898fc65d3fd
Shakti Kumar <shakti.shrivastava13@gmail.com>
2019-11-07 20:29:38 -0500

    update for current value changed to new_val = f(old_val, mean, std)

--- experiment_sin_stability.py
+++ experiment_sin_stability.py
@@ -8,8 +8,11 @@ def f(x, mean, std):
 
 total_size = 1000
 a = list()
+value = f(0, 0, 0.5)
 for el in range(total_size):
-    a.append(f(el/10, 0, 0.5))
+    a.append(value)
+    value = f(value, 0, 0.5)
+
 a = np.array(a)
 plt.plot(np.arange(1,total_size+1), a)
 plt.xlim(100, 200)
@@ -21,7 +24,6 @@ class RandomVariable:
         self.std = 0.5
         self.state = 0
         self.curr_val = 0
-        self.counter = 0
         self.safe_mod = 3
         self.unsafe_mod = 5
 
@@ -40,5 +42,4 @@ class RandomVariable:
         return np.sin(self.curr_val) + np.random.normal(loc=mean, scale=std)
 
     def get_value(self):
-        self.counter += 1
         return self.f(self.mean, self.std)
