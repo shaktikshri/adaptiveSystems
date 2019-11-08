397ea831e83be325aa4dcdfb74dff029d8828a99
Shakti Kumar <shakti.shrivastava13@gmail.com>
2019-11-07 20:23:26 -0500

    get the current state

--- experiment_sin_stability.py
+++ experiment_sin_stability.py
@@ -12,5 +12,32 @@ for el in range(total_size):
     a.append(f(el/10, 0, 0.5))
 a = np.array(a)
 plt.plot(np.arange(1,total_size+1), a)
-plt.xlim(0,100)
+plt.xlim(100, 200)
 
+
+class RandomVariable:
+    def __init__(self):
+        self.max = None
+        self.min = None
+        self.mean = None
+        self.var = None
+        self.state = None
+        self.curr_val = None
+        self.counter = 0
+        self.safe_mod = 3
+        self.unsafe_mod = 5
+
+    def get_state(self):
+        # defined 3 states, safe (0), unsafe (1), critical (2)
+        # Safe : value hops between [-3, +3]
+        # unsafe : value between [-5,-3) and (+3, +5]
+        # critical : values beyond that
+
+        if abs(self.curr_val) < self.safe_mod:
+            return 0
+        elif abs(self.curr_val) < self.unsafe_mod:
+            return 1
+        return 2
+
+    def step(self):
+        self.counter += 1
