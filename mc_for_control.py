c85b56cbecc0aef5e718553f2b9dd18ec690d03e
Shakti Kumar <shakti.shrivastava13@gmail.com>
2019-11-12 09:39:03 -0500

    minor fixes in mc for control

--- mc_for_control.py
+++ mc_for_control.py
@@ -8,7 +8,7 @@ from gridworld import GridWorld
 
 def get_return(state_list, gamma):
     """
-    :param state_list: a list of tuples (observation, state, reward)
+    :param state_list: a list of tuples (observation, action, reward)
     :param gamma: the discount factor
     :return: the return value for that state_list
     """
@@ -37,9 +37,9 @@ def print_policy(p, shape):
         for col in range(shape[1]):
             if(p[row][col] == -1): policy_string += " *  "
             elif(p[row][col] == 0): policy_string += " ^  "
-            elif(p[row][col] == 1): policy_string += " <  "
+            elif(p[row][col] == 1): policy_string += " >  "
             elif(p[row][col] == 2): policy_string += " v  "
-            elif(p[row][col] == 3): policy_string += " >  "
+            elif(p[row][col] == 3): policy_string += " <  "
             elif(np.isnan(p[row][col])): policy_string += " #  "
         policy_string += '\n'
     print(policy_string)
@@ -49,76 +49,22 @@ def update_policy(episode_list, policy_matrix, state_action_matrix):
     """
     Updates the policy in a greedy way, selecting actions which have the highest
     utility for each state visited in the episode_list
-    :param episode_list: the tuples of states visited as (observation, state, reward)
+    :param episode_list: the tuples of states visited as (observation, action, reward)
     :param policy_matrix: the policy matrix
     :param state_action_matrix: the Q matrix
     :return:
     """
     for visit in episode_list:
         observation = visit[0]
-        column = observation[1] + observation[0] * 4
+        column = observation[0] * 4 + observation[1]
         if policy_matrix[observation[0], observation[1]] != -1:
             # if its not the terminal state
             policy_matrix[observation[0], observation[1]] = np.argmax(state_action_matrix[:, column])
     return policy_matrix
 
 
-def generalized_policy_iteration(policy_matrix, env, gamma, running_mean_matrix,
-                                 state_action_matrix, n_epochs, print_epoch):
-    for epoch in range(n_epochs):
-        episode_list = list()
-        observation = env.reset(exploring_starts=False)
-        # observation is the [row,col] of the current position
-        is_starting = True
-        # length of each episode is 1000
-        for _ in range(1000):
-            action = policy_matrix[observation[0], observation[1]]
-            if action == np.nan:
-                continue
-            # If the episode just started then it is
-            # necessary to choose a random action (exploring starts)
-            # This condition assures to satisfy the exploring starts. T
-            if is_starting:
-                action = np.random.randint(0, 4)
-                is_starting = False
-            # Move one step and get a new observation and the reward
-            new_observation, reward, done = env.step(action)
-            episode_list.append((observation, action, reward))
-            observation = new_observation
-            if done:
-                break
-        # This cycle is the implementation of First-Visit MC.
-        first_visit_done = np.zeros((4, 12))
-        counter = 0
-        # For each state-action stored in the episode list it checks if
-        # it is the first visit and then estimates the return.
-        # This is the Evaluation step of the GPI.
-        for visit in episode_list:
-            observation = visit[0]
-            action = visit[1]
-            column = observation[1] + observation[0] * 4
-            row = int(action)
-            if first_visit_done[row, column] == 0:
-                return_value = get_return(episode_list[counter:], gamma)
-                running_mean_matrix[row, column] += 1
-                state_action_matrix[row, column] += return_value
-                first_visit_done[row, column] = 1
-            counter += 1
-        # Policy update (Improvement)
-        policy_matrix = update_policy(episode_list, policy_matrix, state_action_matrix/running_mean_matrix)
-
-        if epoch % print_epoch == 0:
-            print("State-Action matrix after " + str(epoch) + " iterations:")
-            print(state_action_matrix / running_mean_matrix)
-            print("Policy matrix after " + str(epoch + 1) + " iterations:")
-            print(policy_matrix)
-            print_policy(policy_matrix, (3,4))
-
-    return state_action_matrix/running_mean_matrix
-
-
 env = GridWorld(3, 4)
-gamma = 0.9
+gamma = 0.99
 print_epoch = 10000
 
 state_matrix = np.zeros((3,4))
@@ -147,6 +93,54 @@ state_action_matrix = np.random.random_sample((4, 12))
 running_mean_matrix = np.full((4, 12), 1.0e-12)
 # one row of all states for each action, thus 12 columns for each row
 n_epochs = 500000
-state_action_matrix = generalized_policy_iteration(policy_matrix, env, gamma, running_mean_matrix, state_action_matrix, n_epochs, print_epoch)
+
+for epoch in range(n_epochs):
+    episode_list = list()
+    observation = env.reset(exploring_starts=False)
+    # observation is the [row,col] of the current position
+    is_starting = True
+    # length of each episode is 1000
+    for _ in range(1000):
+        action = policy_matrix[observation[0], observation[1]]
+        # If the episode just started then it is
+        # necessary to choose a random action (exploring starts)
+        # This condition assures to satisfy the exploring starts. T
+        if is_starting:
+            action = np.random.randint(0, 4)
+            is_starting = False
+        # Move one step and get a new observation and the reward
+        new_observation, reward, done = env.step(action)
+        # append what had you observed, and what action did you take resulting in what reward
+        episode_list.append((observation, action, reward))
+        observation = new_observation
+        if done:
+            break
+    # This cycle is the implementation of First-Visit MC.
+    first_visit_done = np.zeros((4, 12))
+    counter = 0
+    # For each state-action stored in the episode list it checks if
+    # it is the first visit and then estimates the return.
+    # This is the Evaluation step of the GPI.
+    for visit in episode_list:
+        observation = visit[0]
+        action = visit[1]
+        column = observation[0] * 4 + observation[1]
+        row = int(action)
+        if first_visit_done[row, column] == 0:
+            return_value = get_return(episode_list[counter:], gamma)
+            running_mean_matrix[row, column] += 1
+            state_action_matrix[row, column] += return_value
+            first_visit_done[row, column] = 1
+        counter += 1
+    # Policy update (Improvement)
+    policy_matrix = update_policy(episode_list, policy_matrix, state_action_matrix/running_mean_matrix)
+
+    if epoch % print_epoch == 0:
+        print("State-Action matrix after " + str(epoch) + " iterations:")
+        print(state_action_matrix / running_mean_matrix)
+        print("Policy matrix after " + str(epoch + 1) + " iterations:")
+        print(policy_matrix)
+        print_policy(policy_matrix, (3,4))
+
 print("Utility matrix after " + str(n_epochs) + " iterations: ")
 print(state_action_matrix)
