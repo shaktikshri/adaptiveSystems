768f889d6187c77f3be2831012b07c55711b63d1
Shakti Kumar <shakti.shrivastava13@gmail.com>
2019-11-30 19:16:34 -0500

    Optimal Policy search before IRL formulation

--- irl.py
+++ irl.py
@@ -3,6 +3,8 @@
 
 import numpy as np
 
+# In[]:
+# form the T matrix
 T_s_a_sbar = np.zeros(shape=(25, 4, 25))
 
 # 5*5 gridworld same as the cleaning robot. With start at (5,5) and end at (4,4)
@@ -25,5 +27,35 @@ for row in range(5):
             # visualize this with action UP
             T_s_a_sbar[state_index, a, possible_next_states[0]] += prob[0]     # state towards UP gets 0.8
             T_s_a_sbar[state_index, a, possible_next_states[1]] += prob[1]     # state to the right gets 0.1
-            T_s_a_sbar[state_index, a, possible_next_states[2]] += prob[2]       # state to the down gets 0
+            T_s_a_sbar[state_index, a, possible_next_states[2]] += prob[2]     # state to the down gets 0
             T_s_a_sbar[state_index, a, possible_next_states[3]] += prob[3]     # state to the left gets 0.1
+
+# In[]:
+# With this Transition Matrix we need to find out the best policy
+reward = np.full((5,5), -0.04)
+reward[-1,-1] = 1
+reward[-2, -1] = -1
+Q = np.random.random((5,5,4))
+gamma = 0.9
+epsilon = 0.1
+for row in range(5):
+    for col in range(5):
+        # epsilon greedy policy search
+        if np.random.random() > epsilon:
+            a = np.argmax(Q[row, col, :])
+        else:
+            a = np.random.choice(4)
+        summation = 0
+        for row_bar in range(5):
+            for col_bar in range(5):
+                summation += T_s_a_sbar[row*5+col, a, row_bar*5+col_bar]*np.max(Q[row_bar, col_bar, :])
+        Q[row, col, a] = reward[row, col] + gamma * summation
+
+
+# In[]:
+# Implementation of Finite State Space IRL.
+# See the paper Algorithms for Inverse Reinforcement Learning Section 3
+
+# Need to formulate an linear program
+import pulp
+
