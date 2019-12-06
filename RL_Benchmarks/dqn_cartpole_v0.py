c6a6a06a365ef72f300e25fc6dcd0265a856fa54
Shakti Kumar <shakti.shrivastava13@gmail.com>
2019-12-06 09:40:01 -0500

    epsilon as a parameter

--- RL_Benchmarks/dqn_cartpole_v0.py
+++ RL_Benchmarks/dqn_cartpole_v0.py
@@ -12,13 +12,13 @@ import matplotlib
 cp_env = gym.make('CartPole-v1')
 
 
-def run_current_policy(env, policy):
+def run_current_policy(env, policy, epsilon):
     cur_state = env.reset()
     total_step = 0
     total_reward = 0.0
     done = False
     while not done:
-        action = policy.select_action(np.array([cur_state]))
+        action = policy.select_action(np.array([cur_state]), epsilon)
         next_state, reward, done, info = env.step(action)
         total_reward += reward
         env.render(mode='rgb_array')
@@ -37,7 +37,7 @@ avg_reward = 0.0
 avg_timestep = 0
 
 # initialize policy and replay buffer
-cp_policy = DQNPolicy(cp_env, lr=cp_alpha, epsilon=cp_epsilon, gamma=cp_gamma)
+cp_policy = DQNPolicy(cp_env, lr=cp_alpha, gamma=cp_gamma)
 replay_buffer = ReplayBuffer()
 cp_start_episode = 0
 
@@ -60,7 +60,7 @@ for episode_i in range(cp_start_episode, cp_start_episode + cp_train_episodes):
 
     while not done:
         # select action
-        action = cp_policy.select_action(cur_state.reshape(1, -1))
+        action = cp_policy.select_action(cur_state.reshape(1, -1), cp_epsilon)
 
         # take action in the environment
         next_state, reward, done, info = cp_env.step(action)
@@ -93,5 +93,5 @@ for episode_i in range(cp_start_episode, cp_start_episode + cp_train_episodes):
     pbar_cp.update()
 cp_start_episode = cp_start_episode + cp_train_episodes
 plot_timesteps_and_rewards(cp_avg_history)
-run_current_policy(cp_env, cp_policy)
+run_current_policy(cp_env, cp_policy, cp_epsilon)
 cp_env.close()
