bbe999828c6cc0eaf4c2389afaaf9b1861b77065
Shakti Kumar <shakti.shrivastava13@gmail.com>
2019-12-06 09:37:16 -0500

    reward changes for success

--- RL_Benchmarks/dqn_mountain_car_v0.py
+++ RL_Benchmarks/dqn_mountain_car_v0.py
@@ -1,42 +1,45 @@
 from dqn import DQNPolicy, ReplayBuffer
 import gym
-from tqdm.autonotebook import tqdm
 
 
-def run_current_policy(policy, env, cur_state):
+def run_current_policy(policy, env, cur_state, epsilon):
     done = False
     total_reward = 0
     timesteps = 0
     while not done:
-        action = policy.select_action(cur_state.reshape(1,-1))
+        action = policy.select_action(cur_state.reshape(1,-1), epsilon)
         next_state, reward, done, info = env.step(action)
         env.render()
         print('reward : ',reward),
         total_reward += reward
         timesteps += 1
+        cur_state = next_state
     print('{} timesteps taken and collected {} reward'.format(timesteps, total_reward))
     return total_reward, timesteps
 
 # In[]:
 
 env = gym.make('MountainCar-v0')
+# env = gym.make('CartPole-v0')
 
 # TODO : Can change these parameters
 lr = 0.001
-epsilon = 0.05
-gamma = 0.9
-hidden_dim = 100
+# TODO : Need to do the epsilon decay
+epsilon = 1
+epsilon_decay = 0.05
+epsilon_min = 0.01
+gamma = 0.99
+hidden_dim = 24
 mod_episode = 10
 
-env_policy = DQNPolicy(env, lr, epsilon, gamma, hidden_dim)
+env_policy = DQNPolicy(env, lr, gamma, hidden_dim)
 replay_buffer = ReplayBuffer()
-total_train_episodes = 150
+total_train_episodes = 500
 
 # play with a random policy
 # run_current_policy(env_policy, env, env.reset())
 
 # In[]:
-progress_bar = tqdm(total=total_train_episodes)
 history = dict({'reward':list(), 'timesteps':list(), 'episodes':list()})
 
 for episode in range(total_train_episodes):
@@ -45,36 +48,52 @@ for episode in range(total_train_episodes):
     ep_reward = 0
     ep_timesteps = 0
     cur_state = env.reset()
+    epsilon = max(epsilon, epsilon_min)
+    max_position = -99
     while not done:
-        action = env_policy.select_action(cur_state.reshape(1, -1))
-        next_state, reward, done, info = env.step(action)
+        action = env_policy.select_action(cur_state.reshape(1, -1), epsilon)
+        next_state, reward, done, _ = env.step(action)
+
+        # Visualize the status
+        if episode % mod_episode == 0:
+            env.render()
+
+        # Keep track of max position
+        if next_state[0] > max_position:
+            max_position = next_state[0]
+
+        # Adjust reward for task completion
+        if next_state[0] >= 0.5:
+            reward += 10
+
         replay_buffer.add(cur_state, action, next_state, reward, done)
 
         # TODO : Change the sample size and check any improvements
         sampled_transitions = replay_buffer.sample()
-        env_policy.update_policy(cur_states=sampled_transitions['cur_states'],
-                                 actions=sampled_transitions['actions'],
-                                 next_states=sampled_transitions['next_states'],
-                                 rewards=sampled_transitions['rewards'],
-                                 dones=sampled_transitions['dones'])
-        # env_policy.update_policy(**sampled_transitions)
+        env_policy.update_policy(**sampled_transitions)
         ep_reward += reward
         ep_timesteps += 1
+
+        cur_state = next_state
+
     history['reward'].append(ep_reward)
     history['timesteps'].append(ep_timesteps)
     history['episodes'].append(episode+1)
     if episode % mod_episode == 0:
-        print('Epoch : ', episode)
-        print('Avg Reward : ', history['reward'][-1])
-        print('TimeSteps : ', history['timesteps'][-1])
-    progress_bar.update()
+        print('Epoch : {} Success : {} Avg Reward : {} Timesteps : {} Max position : {}'.format(
+            episode, max_position >= 0.5, history['reward'][-1], history['timesteps'][-1], max_position))
+
+    # decay the epsilon after every episode
+    epsilon -= epsilon_decay
 
 # In[]:
 
 # Now play again
-run_current_policy(env_policy, env, env.reset())
+run_current_policy(env_policy, env, env.reset(), epsilon)
 env.close()
 
 # In[]:
+import matplotlib
+matplotlib.use('Qt5Agg')
 from plot_functions import plot_timesteps_and_rewards
 plot_timesteps_and_rewards(history)
