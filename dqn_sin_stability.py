b5216ae2170e69edc0cda8a2b820bf604ab59511
Shakti Kumar <shakti.shrivastava13@gmail.com>
2019-12-06 14:14:22 -0500

    converging on fixed actions vs fixed noise

--- dqn_sin_stability.py
+++ dqn_sin_stability.py
@@ -22,21 +22,35 @@ plt.show()
 
 # In[]:
 
+
 class RandomVariable():
-    def __init__(self, errepsilon):
+    class ActionSpace():
+        def __init__(self):
+            self.actions = np.linspace(-0.1, +0.1, 10)
+            self.n = self.actions.shape[0]
+
+        def sample(self):
+            return np.random.choice(self.n)
+
+    def __init__(self, errepsilon,  noise_levels, x_increment=0.01, x_range=1):
         self.y = 0
         self.x = 0
+        self.x_increment = x_increment
+        self.x_range = x_range
+        self.noise_levels = noise_levels
         # TODO : Change these values and check
         self.errepsilon = errepsilon
+        self.observation_space = np.array([2])
+        self.action_space = self.ActionSpace()
 
     def step(self, action):
         self.y += action
         reward = self.get_reward()
-        self.x += 1
-        return np.array([self.x, self.y]), reward
+        self.x = ( self.x + self.x_increment ) % self.x_range
+        return np.array([self.x, self.f()]), reward
 
     def f(self):
-        return np.sin(self.x)
+        return np.sin(self.x) + np.random.choice(self.noise_levels)
 
     def get_reward(self):
         if np.abs(self.f() - self.y) < self.errepsilon:
@@ -45,4 +59,103 @@ class RandomVariable():
         else:
             return -1
 
+    def reset(self):
+        self.x = 0
+        self.y = self.f()
+        return np.array([self.x, self.y])
+
+
 # In[]:
+
+from dqn import DQNPolicy, ReplayBuffer
+max_iterations = 100
+
+
+def run_current_policy(policy, env, cur_state, epsilon):
+    total_reward = 0
+    function_history = list()
+    timesteps = 0
+    for iterations in range(max_iterations):
+        action = policy.select_action(cur_state.reshape(1,-1), epsilon)
+        next_state, reward = env.step(action)
+        function_history.append(cur_state)
+        total_reward += reward
+        timesteps += 1
+        cur_state = next_state
+    print('{} timesteps taken and collected {} reward'.format(timesteps, total_reward))
+    return total_reward, timesteps, function_history
+
+# In[]:
+
+noise = [ -0.07777778, 0.07777778]
+
+env = RandomVariable(0.001, noise)
+
+# TODO : Can change these parameters
+lr = 0.001
+# TODO : Need to do the epsilon decay
+epsilon = 1
+epsilon_decay = 0.05
+epsilon_min = 0.01
+gamma = 0.99
+hidden_dim = 24
+mod_episode = 10
+
+env_policy = DQNPolicy(env, lr, gamma, hidden_dim)
+replay_buffer = ReplayBuffer()
+total_train_episodes = 500
+
+# play with a random policy
+# run_current_policy(env_policy, env, env.reset())
+
+# In[]:
+history = dict({'reward':list(), 'timesteps':list(), 'episodes':list()})
+
+for episode in range(total_train_episodes):
+    done = False
+    # print('Epoch :', episode + 1)
+    ep_reward = 0
+    ep_timesteps = 0
+    cur_state = env.reset()
+    epsilon = max(epsilon, epsilon_min)
+    for iterations in range(max_iterations):
+        action = env_policy.select_action(cur_state.reshape(1, -1), epsilon)
+        next_state, reward = env.step(action)
+
+        replay_buffer.add(cur_state, action, next_state, reward, done)
+
+        # TODO : Change the sample size and check any improvements
+        sampled_transitions = replay_buffer.sample()
+        # the q updation occurs for all transitions in all episodes, just like TD updates
+        env_policy.update_policy(**sampled_transitions)
+        ep_reward += reward
+        ep_timesteps += 1
+
+        cur_state = next_state
+
+    history['reward'].append(ep_reward)
+    history['timesteps'].append(ep_timesteps)
+    history['episodes'].append(episode+1)
+    if episode % mod_episode == 0:
+        # Get last 100 points from replay buffer
+        states = replay_buffer.cur_states[:-100]
+        print('Epoch : {} Avg Reward : {} Timesteps : {}'.format(
+            episode, history['reward'][-1], history['timesteps'][-1]))
+        # plt.figure()
+        plt.scatter(states[0], states[1], label='Episode : '+str(episode))
+        plt.legend()
+        plt.show()
+
+    # decay the epsilon after every episode
+    epsilon -= epsilon_decay
+
+# In[]:
+
+# Now play again
+run_current_policy(env_policy, env, env.reset(), epsilon)
+
+# In[]:
+import matplotlib
+matplotlib.use('Qt5Agg')
+from plot_functions import plot_timesteps_and_rewards
+plot_timesteps_and_rewards(history)
