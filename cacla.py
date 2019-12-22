20d781b75c152016c92b5f19a6719d74a233c6b2
Shakti Kumar <shakti.shrivastava13@gmail.com>
2019-12-22 18:47:16 -0500

    numpy implementation of NN for actor gradient tweaks

--- cacla.py
+++ cacla.py
@@ -54,3 +54,118 @@ def main():
     y_pred = network(Variable(torch.from_numpy(X_test)).float()).data.numpy()
     import matplotlib.pyplot as plt
     plt.plot(X_test, y_pred)
+
+# In[]:
+
+# This update is taken from PyTorch examples in
+# https://github.com/jcjohnson/pytorch-examples/blob/master/README.md#pytorch-defining-new-autograd-functions
+# We want to have a manual update scheme for the parameters where we update the parameters of critic only when the
+# TD error is greater than 0. The update to the actor can be done with the same fucntion approxmiation that we wrote
+# in the beginning.
+# TODO : Need to make the actor and critic concurrent. Implement both of them either in Pytorch or in numpy
+# TODO : but do remember to check for the correctness.
+
+# import numpy as np
+#
+# # N is batch size; D_in is input dimension;
+# # H is hidden dimension; D_out is output dimension.
+# N, D_in, H, D_out = 64, 1, 12, 1
+#
+# # Create random input and output data
+# x = np.random.randn(N, D_in)
+#
+# y = np.power(x, 2)
+#
+# # Randomly initialize weights
+# w1 = np.random.randn(H, D_in)
+# w2 = np.random.randn(D_out, H)
+#
+# learning_rate = 1e-6
+# for t in range(500):
+#     # Forward pass: compute predicted y
+#     h = x.dot(w1)
+#     h_relu = np.maximum(h, 0)
+#     y_pred = h_relu.dot(w2)
+#
+#     loss = np.square(y_pred - y).sum()
+#     print('Iteration : ', t, 'Loss : ', loss)
+#
+#     grad_y_pred = 2.0 * (y_pred - y)
+#     grad_w2 = h_relu.T.dot(grad_y_pred)
+#     grad_h_relu = grad_y_pred.dot(w2.T)
+#     grad_h = grad_h_relu.copy()
+#     grad_h[h < 0] = 0
+#     grad_w1 = x.T.dot(grad_h)
+#
+#     # Update weights
+#     w1 -= learning_rate * grad_w1
+#     w2 -= learning_rate * grad_w2
+
+
+
+# In[]:
+
+import numpy as np
+learning_rate = 0.01
+
+X = np.arange(-1, 1, 0.001).reshape(-1, 1)
+X_new = np.ones((X.shape[0], X.shape[1]+1))
+X_new[:, 1] = np.squeeze(X)
+X = X_new
+
+t = np.power(X[:,1], 2).reshape(-1, 1)
+input_dim = X.shape[1]
+hidden_dim = 12
+output_dim = 1
+
+# TODO : Use autograd to find this out
+W1 = np.random.randn(hidden_dim, input_dim)
+W2 = np.random.randn(output_dim, hidden_dim)
+# TODO : Can change the activation function as well
+relu = lambda z : np.maximum(z, 0)
+sigma = lambda z : 1/(1+np.exp(-z))
+relu_dash = lambda z: np.where(z > 0, 1, 0)
+
+loss_array = list()
+for epoch in range(100000):
+    # forward pass
+    Z = np.dot(X, W1.T)
+    H = relu(Z)
+    y = np.dot(H, W2.T)
+    L = (1/X.shape[0]) * np.sum(np.power(y-t, 2))
+    # print('Epoch ', epoch, 'Loss : ', L)
+    loss_array.append(L)
+    # backward pass
+    L_bar = 1
+    y_bar = (1/X.shape[0]) * L_bar * (y-t)
+    H_bar = np.dot(y_bar, W2)
+    W2_bar = np.dot(y_bar.T, H)
+    Z_bar = H_bar * relu_dash(Z)
+    W1_bar = np.dot(Z_bar.T, X)
+
+    # Update the Gradient
+    W1 -= learning_rate * W1_bar
+    W2 -= learning_rate * W2_bar
+
+    if epoch % 1000 == 0:
+        print('Epoch : ', epoch, ' Loss : ', L, 'Learning Rate : ', learning_rate)
+        learning_rate = min(learning_rate, np.exp(-epoch//1000))
+        learning_rate = max(1e-05, learning_rate)
+
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Qt5Agg')
+plt.plot(loss_array)
+
+# In[]:
+
+X = np.arange(-1, 1, 0.001).reshape(-1, 1)
+X_new = np.ones((X.shape[0], X.shape[1]+1))
+X_new[:, 1] = np.squeeze(X)
+X = X_new
+
+Z = np.dot(X, W1.T)
+H = relu(Z)
+y = np.dot(H, W2.T)
+
+plt.plot(X[:, 1], y)
