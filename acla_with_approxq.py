5fd505127e21fc400c6ce34893a6dfb0e2692d95
Shakti Kumar <shakti.shrivastava13@gmail.com>
2019-12-27 17:12:58 -0500

    best performance till 27th dec, TD error with moving target and decaying gamma

--- acla_with_approxq.py
+++ acla_with_approxq.py
@@ -8,7 +8,7 @@ import numpy as np
 from torch.optim.lr_scheduler import StepLR
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from actor_critic_structure import Actor, Critic
-from  copy import deepcopy
+from copy import deepcopy
 
 # In[]:
 # TODO :
@@ -20,17 +20,17 @@ from  copy import deepcopy
 
 # TODO : Tweaking the learning rate
 actor_learning_rate = 1e-2
-critic_learning_rate = 1e-3
+critic_learning_rate = 1e-4
 train_episodes = 5000
 
 env = gym.make('CartPole-v1')
-actor = Actor(input_size=env.observation_space.shape[0], output_size=env.action_space.n)
+actor = Actor(input_size=env.observation_space.shape[0], output_size=env.action_space.n, hidden_size=24)
 
 # Approximating the Value function
-critic = Critic(input_size=env.observation_space.shape[0], output_size=1)
+critic = Critic(input_size=env.observation_space.shape[0], output_size=1, hidden_size=24)
 # critic_old is used for fixing the target in learning the V function
 critic_old = deepcopy(critic)
-copy_epoch = 500
+copy_epoch = 100
 
 optimizer_algo = 'sgd'
 
@@ -46,7 +46,7 @@ elif optimizer_algo == 'batch':
 # gamma = decaying factor
 # TODO : Can change the step size
 actor_scheduler = StepLR(actor_optimizer, step_size=500, gamma=0.1)
-critic_scheduler = StepLR(critic_optimizer, step_size=100, gamma=0.1)
+critic_scheduler = StepLR(critic_optimizer, step_size=200, gamma=0.1)
 
 
 gamma = 0.99
@@ -67,7 +67,10 @@ replay_buffer = ReplayBuffer()
 def update_critic(cur_states, actions, next_states, rewards, dones):
     # target doesnt change when its terminal, thus multiply with (1-done)
     # target = R(st-1, at-1) + gamma * max(a') Q(st, a')
+
+    # move towards the fixed target, as per the older critic's prediction
     targets = rewards + torch.mul(1 - dones, gamma*critic(next_states).squeeze(-1) )
+
     # expanded_targets are the Q values of all the actions for the current_states sampled
     # from the previous experience. These are the predictions
     expanded_targets = critic(cur_states).squeeze(-1)
@@ -108,8 +111,7 @@ for episode_i in range(train_episodes):
         u_value = critic(cur_state)
         # Update parameters of critic by TD(0)
         # TODO : Use TD Lambda here and compare the performance
-        # move towards the fixed target, as per the older critic's prediction
-        target = reward + gamma * critic_old(cur_state)
+        target = reward + gamma * critic(next_state)
 
         replay_buffer.add(cur_state, action, next_state, reward, done)
         # sample minibatch of transitions from the replay buffer
@@ -123,6 +125,7 @@ for episode_i in range(train_episodes):
             actor_optimizer.zero_grad()
             # compute the gradient from the sampled log probability
             #  the log probability times the Q of the action that you just took in that state
+            # TODO : the target here is still a moving target, see if fixing this for sometime leads to any improvement
             loss2 = -log_prob * (target - u_value) # the advantage function used is the TD error
             loss2.backward()
             running_loss2_mean += loss2.item()
@@ -132,6 +135,24 @@ for episode_i in range(train_episodes):
             target_list = torch.cat([target_list, target])
             u_value_list = torch.cat([u_value_list, u_value])
             log_prob_list = torch.cat([log_prob_list, log_prob.reshape(-1)])
+
+
+
+
+        # TODO : This has to be removed, this form of training is not viable at all!
+        if optimizer_algo == 'batch':
+            # Update parameters of actor by policy gradient
+            actor_optimizer.zero_grad()
+            # compute the gradient from the sampled log probability
+            #  the log probability times the Q of the action that you just took in that state
+            loss2 = torch.sum(
+                torch.mul(-log_prob_list, target_list - u_value_list))  # the advantage function used is the TD error
+            loss2.backward()
+            running_loss2_mean += loss2.item()
+            actor_optimizer.step()
+
+
+
 
         episode_reward += reward
         episode_timestep += 1
@@ -165,14 +186,14 @@ for episode_i in range(train_episodes):
     critic_scheduler.step()
 
     if (episode_i + 1) % agg_interval == 0:
-        # print('Episode : ', episode_i+1,
-        #       'actor lr : ', actor_scheduler.get_lr(), 'critic lr : ', critic_scheduler.get_lr(),
-        #       'Actor Loss : ', loss2_history[-1], 'Critic Loss', loss1_history[-1],
-        #       'Avg Timestep : ', avg_history['timesteps'][-1])
         print('Episode : ', episode_i+1,
-            'critic lr : ', critic_scheduler.get_lr(),
-            'Critic Loss', loss1_history[-1],
-            'Avg Timestep : ', avg_history['timesteps'][-1])
+              'actor lr : ', actor_scheduler.get_lr(), 'critic lr : ', critic_scheduler.get_lr(),
+              'Actor Loss : ', loss2_history[-1], 'Critic Loss', loss1_history[-1],
+              'Avg Timestep : ', avg_history['timesteps'][-1])
+        # print('Episode : ', episode_i+1,
+        #     'critic lr : ', critic_scheduler.get_lr(),
+        #     'Critic Loss', loss1_history[-1],
+        #     'Avg Timestep : ', avg_history['timesteps'][-1])
 
 
 # In[]:
