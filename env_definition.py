819e6d60aebbd4043c4f4f88c5072d6fb9918b18
Shakti Kumar <shakti.shrivastava13@gmail.com>
2020-01-01 14:24:41 -0500

    added circular timestep for agent to see previous states again

--- env_definition.py
+++ env_definition.py
@@ -7,13 +7,18 @@ import numpy as np
 
 
 class RandomVariable:
-    def __init__(self):
+    def __init__(self, highest, intermediate, penalty, lowest):
         self.state = 0
         self.time = 0
-        self.timestep = 0.1
+        self.timestep = 0.5
         self.cur_state = self.f(self.time) # the function is currently in one dimension, we can raise this dimension
         self.observation_space = np.array(1).reshape(-1)
         self.action_space = np.array(1).reshape(-1)
+        self.highest = highest
+        self.intermediate = intermediate
+        self.lowest = lowest
+        self.penalty = penalty
+        self.max_time = 2*np.pi
 
     def get_noise(self):
         # TODO: Replace this with a gaussian, np.random.normal(0, 0.1)
@@ -31,15 +36,17 @@ class RandomVariable:
     def step(self, action):
         difference = np.abs(self.f(self.time) - (self.cur_state + action))
         done = False
-        reward = -99
         if difference <= 0.01:
-            reward = +1
+            reward = self.highest
         elif difference <= 0.1:
-            reward = -1
+            reward = self.intermediate
+        elif difference <= 0.7:
+            reward = self.penalty
         # a max deviation between -0.1 to +0.1 is tolerated, after that the episode ends
         else:
+            reward = self.lowest
             done = True
         # Thus reward directly depends on how good the approximation was
-        self.time += self.timestep
+        self.time = (self.time + self.timestep) % self.max_time
         self.cur_state = self.f(self.time) + self.get_noise()
         return self.cur_state, reward, done, 'info'
